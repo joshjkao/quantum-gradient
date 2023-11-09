8aa7e87e003413a12362ef8e3715ee4c7b0647f1
Joshua Kao <98560037+joshjkao@users.noreply.github.com>
2023-11-08 20:59:10 -0800

    first implementation

---
+++ util.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def binaryArray2Int(x):
+    y = 0
+    for i,j in enumerate(reversed(x)):
+        y += j<<i
+    return y
+
+def int2BinaryArray(x, length):
+    ret = [int(b) for b in bin(x)[2:]]
+    while len(ret) < length:
+        ret.insert(0, 0)
+    return ret
